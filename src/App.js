2a7563319ccc5346fb5ef367ea5cebc687f23a50
Mohit Gupta <mg6489@srmist.edu.in>
2021-11-19 23:02:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,36 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import youtube from './api/youtube';
+import SearchBar from './components/SearchBar';
 
 
-function App() {
+class App extends React.Component{
+  state={
+    video:[],
+    selectedVideo:null,
+  }
+
+  handleSubmit = async (searchTerm)=>{
+  const response = await youtube.get('search',{
+    params: {
+      part: 'snippet',
+      maxResults: 5,
+      key: 'AIzaSyArWmMr6nxGpgi-FqoalEIsBWHGSS_-jPE',
+      q: searchTerm,
+  }});
+
+
+  console.log(response);
+  //this.setState({video:response.data.item,selectedVideo:response.data.item[0]})
+
+  }
+  render(){
   return (
-    <Grid justify="center" container spacing={16}>
+    <Grid justifyContent="center" container spacing={10}>
       <Grid item xs={12}>
-        <Grid container spacing={16}>
+        <Grid container spacing={10}>
           <Grid item xs={12}>
-            {/*Searchbar component */}
+            <SearchBar onFormSubmit={this.handleSubmit} />
           </Grid>
           <Grid item xs={8}>
             {/*VideoDetail component */}
@@ -24,6 +45,7 @@ function App() {
     </Grid>
     
   );
+};
 }
 
 export default App;
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-baseURL:'http://googlepapis.com/youtube/v3',
-params:{
-    parts:'snippet',
-    maxResult:5,
-    key:'AIzaSyArWmMr6nxGpgi-FqoalEIsBWHGSS_-jPE'
+baseURL:'https://www.googlepapis.com/youtube/v3',
+
 }
 
-
-});
+);
  
